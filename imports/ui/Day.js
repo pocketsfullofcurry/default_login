d7ab62956ed0763675e9a7243bb2951ac7c33203
beezus <bimalpa.work@gmail.com>
2018-01-19 13:21:56 -0500

    date spacing fixed for months

--- imports/ui/Day.js
+++ imports/ui/Day.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const DayBtn = (props) => {
+export default class DayBtn extends React.Component{
+  constructor(props){
+    super(props);
+  }
+renderMonth() {
+  if(this.props.hour.length > 2) {
+    let btnArray = this.props.hour.split(" ");
+      return <button className={this.props.coco}>{btnArray[0]}<br/>{btnArray[1]}</button>
+  } else {
+      return <button className={this.props.coco}>{this.props.hour}</button>
+  }
+}
+render(){
   return (
-
-      <button className={props.coco}>{props.hour}</button>
-
+    this.renderMonth()
   )
-};
+}
 
-export default DayBtn;
+}
