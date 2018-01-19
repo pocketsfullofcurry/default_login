7bfc8309eb2046285b0c44a226406fb1a1205a1e
beezus <bimalpa.work@gmail.com>
2018-01-18 22:30:46 -0500

    tester with big numbers

--- imports/ui/Tester.js
+++ imports/ui/Tester.js
@@ -6,11 +6,13 @@ export default class Tester extends React.Component{
     return (
       <div>
         <div>
+        <Day hour="1" coco="day blue"/>
         <Day hour="2" coco="day red"/>
         <Day hour="3" coco="day blue"/>
         <Day hour="4" coco="day green"/>
         <Day hour="5" coco="day yellow"/>
         <Day hour="6" coco="day pink"/>
+        <Day hour="7" coco="day red"/>
         </div>
       </div>
     );
