897a74ba7e083030944fb64923171587daf6685a
beezus <bimalpa.work@gmail.com>
2018-01-18 22:20:48 -0500

    added tester

---
+++ imports/ui/Day.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Daye = (props) => {
+  return (
+
+      <button className={props.coco}>{props.hour}</button>
+
+  )
+};
+
+export default Daye;
--- imports/ui/Tester.js
+++ imports/ui/Tester.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import Day from './Day';
 
 export default class Tester extends React.Component{
   render(){
     return (
       <div>
-      <button>10</button>
-      <button>12</button>
-      <button>13</button>
-      <button>14</button>
-      <button>15</button>
-      <button>16</button>
-      <button>17</button>
-      <button>18</button>  
+        <div>
+        <Day hour="2" coco="day red"/>
+        <Day hour="3" coco="day blue"/>
+        <Day hour="4" coco="day green"/>
+        <Day hour="5" coco="day yellow"/>
+        <Day hour="6" coco="day pink"/>
+        </div>
       </div>
     );
   };
